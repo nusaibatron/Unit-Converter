c19ae1584c58bf9f9c365eb3bc5fe8a8a8b04b79
nusaibatron <nusaiba@berkeley.edu>
2022-08-12 01:37:39 -0400

    Unit converter first commit

--- Unit Converter/ContentView.swift
+++ Unit Converter/ContentView.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var inputWeight = 0
+    
+    @State private var unit = "metric"
+    private var unitOptions = ["metric", "US"]
+    
+    var performConversion = { (weight: Double, unit: String) -> Double in
+        switch unit {
+        case "US":
+            return 2.2 * weight
+        default:
+            return 0.454 * weight
+
+        }
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Chosen System", selection: $unit) {
+                        ForEach(unitOptions, id: \.self) {
+                            Text($0)
+                        }
+                        
+                    } .pickerStyle(.segmented)
+                    
+                    
+                } header: {
+                    Text("Which system are we converting to?")
+                }
+                Section {
+                    TextField("Weight", value: $inputWeight, format: .number)
+                        .keyboardType(.decimalPad)
+
+                } header: {
+                    Text("Input weight")
+
+                }
+                
+                Section {
+                    let convertedWeight = performConversion(Double(inputWeight), unit)
+                    Text("\(convertedWeight)")
+                } header: {
+                    Text("Converted weight")
+                }
+            } .navigationTitle("Unit Converter")
+        } .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Spacer()
+                Button("Done") {
+                }
+            }
+        }
     }
 }
 
